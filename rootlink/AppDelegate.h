dcd07bdd591d901402ecad3a51343878d0fd9d69
Mr.Meng <406719352@qq.com>
2018-07-25 07:34:53 +0800

    Initial Commit

---
+++ rootlink/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rootlink
+//
+//  Created by cocoa on 2018/7/25.
+//  Copyright © 2018年 rootlink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
